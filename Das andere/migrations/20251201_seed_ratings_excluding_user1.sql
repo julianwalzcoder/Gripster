4624c7cbb24416d18f900efc26edd24e254ad9f2
Julian Walz <julianwalz@Julians-MacBook-Pro-3.local>
2025-12-02 23:42:50 +0100

    Moved ratings from SessionRoute to UserRoute, implemented average function in RouteController, added Average to frontend; removed 'Das andere' folder

--- Das andere/migrations/20251201_seed_ratings_excluding_user1.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
--- Setze Ratings für alle UserRoute-Einträge außer UserID=1, falls Rating fehlt/ungültig
-UPDATE public."UserRoute" ur
-SET "Rating" = (floor(random()*5)::int + 1)
-WHERE ur."UserID" <> 1
-  AND (ur."Rating" IS NULL OR ur."Rating" NOT BETWEEN 1 AND 5);
-
-COMMIT;
